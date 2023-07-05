5e0aa6f07de9fb45606548d74ef25233f1933757
Osele Chidera <104494952+OseleChidera@users.noreply.github.com>
2023-07-05 20:38:48 +0100

    Update sneakers.js

--- sneakers.js
+++ sneakers.js
@@ -8,13 +8,18 @@ let productMainName = document.getElementById("product-title").innerHTML;
 let productPrice = document.getElementById("main-price").innerHTML.replace("$", "");
 let AddToCartBtn = document.getElementById("add-to-cart");
 let finalQuantity = Number(counter_value.value);
-let cartIcon = document.getElementById("cart-icon-main");
+let cartDiv = document.getElementById("cart-div");
+let cartDivM = document.getElementById("cart-div-m");
 let counterValue = 0;
 let allDeleteButtons = document.getElementsByClassName("delete");
 let cartArray = [];
 let index = 0
 let cartParentElement = document.querySelector(".cart-list")
-    //Counter functionality
+const burgerMenu = document.getElementById('burger-toggle')
+const bottomMenu = document.getElementById('bottom')
+let cartFloater = document.getElementById('cart-floater')
+
+//Counter functionality
 add.addEventListener("click", function add() {
     counterValue += 1
     counter_value.value = counterValue
@@ -33,84 +38,148 @@ sub.addEventListener("click", function subtract() {
 //changing the src using preset images from the thumbnails
 thumbnailArray.forEach((e) => {
     e.addEventListener("click", function changeMainImageOnclick() {
+        thumbnailArray.forEach((item) => {
+            item.classList.remove('thumbnail-active');
+        });
+        this.classList.add('thumbnail-active')
         let thumbnailSrcWithReplacedText = this.getAttribute("src").replace("-thumbnail", '')
         main_image.setAttribute('src', thumbnailSrcWithReplacedText)
     })
 })
-
+document.addEventListener('click', (event) => {
+    // Check if the click is outside the items
+    if (!event.target.classList.contains('thumbnail')) {
+        // Remove focus from all items
+        thumbnailArray.forEach((item) => {
+            item.classList.remove('thumbnail-active');
+        });
+    }
+});
 
 //using an array of objects to pass data using js template strings 
 AddToCartBtn.addEventListener("click", function addItemsToCart() {
+    // let cartFloater = document.querySelector('.cart-floater')
+
     if (!Number(counter_value.value)) {
         console.log("you have to have something in your cart");
         counter_value.classList.add("red")
         return;
-    }
-    cartArray.push(
-        `<div class="item" id=${index}>
-        <div><img src=${main_image.getAttribute('src')} alt="" srcset="" class="item-picture"></div>
-        <div class="item-description">
-        <h2 class="product-title" id="product-title">${productMainName}</h2>
-        <h3 class="main-price" id="main-price-cart">$${Number(productPrice)} x ${counter_value.value} = $${Number(productPrice) * Number(counter_value.value)}</h3>
-        </div>
-        <img src="icon-delete.svg" class="delete delete-div" alt="delete" srcset="">
-        </div>`)
-        counter_value.classList.remove("red")
 
-        const jsArray = cartArray.from(jsxArray);
-        jsArray.forEach((item , index)=>{cartParentElement.appendChild(item)})
+    }
+
+    cartArray.push({
+        productName: productMainName,
+        productImage: main_image.getAttribute('src'),
+        productQuantity: counter_value.value,
+        productPrices: Number(productPrice)
+    })
+    createElements(cartArray);
+    counter_value.classList.remove("red")
+    console.log(cartArray);
+    cartFloater.classList.add('cart-floater-shown')
 })
 
 
-// function createElements(cartArray) {
-//     cartArr.forEach((item , index)=>{
-//     cartParentElement.appendChild(item);
-//     })
+function createElements(wholeCartArray) {
+    const cartArr = wholeCartArray.map((eachItem, index) => {
+        return `
+        <div class="item" id=${index}>
+            <div><img src=${eachItem.productImage} alt="" srcset="" class="item-picture"></div>
+            <div class="item-description">
+            <h2 class="product-title" id="product-title">${eachItem.productName}</h2>
+            <h3 class="main-price" id="main-price-cart">$${eachItem.productPrices} x ${eachItem.productQuantity} = $${eachItem.productPrices * Number(eachItem.productQuantity)}</h3>
+            </div>
+            <img src="icon-delete.svg" class="delete delete-div" alt="delete" id=${index}>
+            </div>`
+    })
+    cartParentElement.innerHTML = cartArr.join('');
 
-
-
-
-
-    // const cartArr = cartArray.map((eachItem , index) => {
-    // return (`<div class="item" id=${index}>
-    //         <div><img src=${eachItem.productImage} alt="" srcset="" class="item-picture"></div>
-    //         <div class="item-description">
-    //         <h2 class="product-title" id="product-title">${eachItem.productName}</h2>
-    //         <h3 class="main-price" id="main-price-cart">$${eachItem.productPrices} x ${eachItem.productQuantity} = $${eachItem.productPrices * Number(eachItem.productQuantity)}</h3>
-    //         </div>
-    //         <img src="icon-delete.svg" class="delete delete-div" alt="delete" srcset="">
-    //         </div>`)
-    // })
-    // cartParentElement.innerHTML = cartArr;
 
 
     document.querySelectorAll(".delete").forEach((delButton) => {
-        delButton.addEventListener('click', function print(event, ) {
-            console.log('delete button was clicked !!!')
-            const delButton = this.parentElement;
-            const updatedItems = [...cartItem].filter((item , index) => item.id !== delButton.id);
-            cartParentElement.innerHTML = updatedItems;
-            // delButton.remove()
+        delButton.addEventListener('click', function print(event,) {
+            const relativeParent = this.parentElement;
+            cartArray.splice(Number(relativeParent.id), 1)
+            relativeParent.remove()
 
-
-        //   let cartItem = cartParentElement.children;
-        //   const updatedItems = [...cartItem].filter((item , index) => item.id !== index);
-        //   cartParentElement.innerHTML = ''; // Clear the existing items
-        //   updatedItems.forEach(item => cartParentElement.appendChild(item)); 
-           
         })
     })
-// }
+}
+
 
 // ========================================================================
 
 
+// toggle cart icon using the onclick event
 
-//by default the cart list is set to display none this toggles the display using the onclick event
-cartIcon.addEventListener("click", function showCartItems() {
-    let wholeContainer = this.parentNode.querySelector("div")
-    if (wholeContainer.style.display === "none") {
-        wholeContainer.style.display = "flex"
-    } else wholeContainer.style.display = "none"
-
+cartDiv.addEventListener("click", function () {
+    console.log('cart icon was clickweened')
+    let cartFloater = document.getElementById('cart-floater')
+    cartFloater.classList.toggle('cart-floater-shown')
 })
+
+
+
+
+
+
+
+burgerMenu.addEventListener('click', function () {
+    console.log('checkbox checked')
+    if (burgerMenu.checked) {
+        bottomMenu.classList.add("bottom-checked")
+    } else {
+        bottomMenu.classList.remove("bottom-checked")
+    }
+});
+
+
+
+const prevButton = document.getElementById('prev')
+const nextButton = document.getElementById('next')
+const mainPopupImage = document.getElementById('main-image-popup')
+let tracker = 0;
+const popupImgArray = ['./images/image-product-1.jpg', './images/image-product-2.jpg', './images/image-product-3.jpg', './images/image-product-4.jpg']
+
+const mainImage = document.getElementById('product-main-image')
+
+mainImage.addEventListener('click', () => {
+    console.log('clg nigga')
+    document.getElementById('main-right-dialog').style.display = "flex"
+})
+
+
+// popupThumbnailArray[tracker].classList.add('thumbnail-active')
+nextButton.addEventListener('click', function () {
+    tracker++;
+    if (tracker >= popupImgArray.length) {
+        tracker = 0
+    }
+    showActiveThumbnail()
+    mainPopupImage.setAttribute('src', popupImgArray[tracker])
+});
+
+prevButton.addEventListener('click', function () {
+    tracker--;
+    if (tracker < 0) {
+        tracker = popupImgArray.length - 1;
+    }
+    showActiveThumbnail()
+    mainPopupImage.setAttribute('src', popupImgArray[tracker])
+});
+
+document.getElementById('close-dialog').addEventListener('click', function (event) {
+    document.getElementById('main-right-dialog').style.display = 'none'
+});
+
+const popupThumbnails = document.getElementsByClassName("popup-Thumbnail")
+const popupThumbnailArray = Array.from(popupThumbnails)
+
+
+function showActiveThumbnail() {
+    popupThumbnailArray.forEach(eachThumbnail => {
+        eachThumbnail.classList.remove('thumbnail-active');
+        popupThumbnailArray[tracker].classList.add('thumbnail-active')
+
+    })
+}
